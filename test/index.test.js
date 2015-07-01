234deeaef707b5408f4d5b0e4fec5c638e4d2d1d
Nathan Houle <nathan@nathanhoule.com>
2015-06-30 19:03:15 -0700

    Release 1.0.3

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 
-var Analytics = require('analytics.js').constructor;
+var Analytics = require('analytics.js-core').constructor;
 var integration = require('analytics.js-integration');
 var is = require('is');
 var sandbox = require('clear-env');
